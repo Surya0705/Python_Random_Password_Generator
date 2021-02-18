eb983a7b88c812b714fa730c7505d58f72ca9813
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-02-18 23:46:41 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,5 @@
+from password_generator import PasswordGenerator # Importing the module.
+
+a = PasswordGenerator() # Initialising the generator.
+b = a.generate() # Genearting a strong Password.
+print(f"Your generated Password is {b}") # Displaying the Password to the User.
